1ecbf3216fda5a3dbc0b633cdbf7724c9299be3b
Rodolfo Alves <rodolfoalveseng@gmail.com>
2021-09-08 20:53:31 -0300

    Group by search

--- backend/src/main/java/com/devsuperior/dsvendas/service/SaleService.java
+++ backend/src/main/java/com/devsuperior/dsvendas/service/SaleService.java
@@ -4,6 +4,8 @@ package com.devsuperior.dsvendas.service;
 
 
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -11,6 +13,8 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.devsuperior.dsvendas.dto.SaleDTO;
+import com.devsuperior.dsvendas.dto.SalesSucessDTO;
+import com.devsuperior.dsvendas.dto.SalesSumDTO;
 import com.devsuperior.dsvendas.entities.Sale;
 import com.devsuperior.dsvendas.repositories.SaleRepository;
 import com.devsuperior.dsvendas.repositories.SellerRepository;
@@ -29,6 +33,16 @@ public class SaleService {
 		sellerRepository.findAll(); //buscando os sellers e armazenando em memoria para evitar interações repetidas ao banco de dados
 		Page<Sale> result = repository.findAll(pageable);
 		return result.map(x -> new SaleDTO(x)); //page não precisa do stream pois ele já é um stream
+	}
+	
+	@Transactional(readOnly = true)
+	public List<SalesSumDTO> amountGroupedBySeller(){
+	return repository.amountGroupedBySeller();
+	}
+	
+	@Transactional(readOnly = true)
+	public List<SalesSucessDTO> sucessGroupedBySeller(){
+	return repository.sucessGroupedBySeller();
 	}
 
 }
