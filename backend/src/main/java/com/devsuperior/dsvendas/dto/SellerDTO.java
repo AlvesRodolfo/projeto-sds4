5d619fc78cef9646fb50d099fd39d37bd44e4801
Rodolfo Alves <rodolfoalveseng@gmail.com>
2021-09-08 17:55:11 -0300

    Layers

---
+++ backend/src/main/java/com/devsuperior/dsvendas/dto/SellerDTO.java
@@ -0,0 +1,41 @@
+package com.devsuperior.dsvendas.dto;
+
+import java.io.Serializable;
+
+import com.devsuperior.dsvendas.entities.Seller;
+
+public class SellerDTO implements Serializable { //garante que os objetos possam trafegar em rede (são convertidos para byte), serem salvos em arquivos
+	
+	private static final long serialVersionUID = 1L;
+	private Long id;
+	private String name;
+	
+	public SellerDTO() {
+	}
+
+	public SellerDTO(Long id, String name) {
+		this.id = id;
+		this.name = name;
+	}
+	
+	public SellerDTO(Seller entity) { //construtor para passar facilmente os dados da entidade para o SellerDTO
+		id = entity.getId();
+		name = entity.getName();
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+}
